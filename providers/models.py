80de82fc97cb118c5debb39d9a030df197904ce1
revisto <walt.shabani@gmail.com>
2021-09-03 14:10:49 +0430

    add Provider model

--- providers/models.py
+++ providers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Provider(models.Model):
+    name = models.CharField("name", max_length=255)
+    email = models.EmailField(max_length=255)
+    phone_number = models.IntegerField("phone")
+    language = models.CharField("language", max_length=255)
+    currency = models.CharField("currency", max_length=10)
+    
+    def __str__(self):
+        return self.name
