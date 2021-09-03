dee14a980f1ef0c2e0cf099028bc4c98f5cffc42
revisto <walt.shabani@gmail.com>
2021-09-03 15:51:59 +0430

    remove providers prefix

--- mozio/urls.py
+++ mozio/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('providers/', include('providers.urls')),
+    path('', include('providers.urls')),
 ]
