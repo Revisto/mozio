0640f0fd3b43ba0825ff0730de55d2a816a94a14
revisto <walt.shabani@gmail.com>
2021-09-03 15:29:06 +0430

    add view set for ProviderServiceArea

--- providers/views.py
+++ providers/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 
-from .serializers import ProviderSerializer
-from .models import Provider
+from .serializers import ProviderSerializer, ProviderServiceAreaSerializer
+from .models import Provider, ProviderServiceArea
 
 
 class ProviderViewSet(viewsets.ModelViewSet):
     queryset = Provider.objects.all().order_by('name')
     serializer_class = ProviderSerializer
+
+class ProviderServiceAreaViewSet(viewsets.ModelViewSet):
+    queryset = ProviderServiceArea.objects.all().order_by('name')
+    serializer_class = ProviderServiceAreaSerializer
