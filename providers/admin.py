44f43d1e9b8ef9f186b1434435a3ecb865f1af41
revisto <walt.shabani@gmail.com>
2021-09-03 14:26:50 +0430

    register models

--- providers/admin.py
+++ providers/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Provider, ProviderServiceArea
 
 # Register your models here.
+admin.site.register(Provider)
+admin.site.register(ProviderServiceArea)
