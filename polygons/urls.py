4b02f067a5516c2281f7545db1ba8826f98b2632
revisto <walt.shabani@gmail.com>
2021-09-05 02:31:06 +0430

    add PolygonsPoint to urls

--- polygons/urls.py
+++ polygons/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
+
+
 from . import views
 
 router = routers.DefaultRouter()
@@ -8,4 +10,5 @@ router.register(r'polygons', views.ProviderServiceAreaViewSet)
 # Wire up our API using automatic URL routing.
 urlpatterns = [
     path('', include(router.urls)),
+    path('polygon-point', views.PolygonsPoint.as_view()),
 ]
